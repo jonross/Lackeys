4ff4fd6a018d91d5100edba60dfc1275b8bfa1d7
Jonathan Ross <jon.ross@pathai.com>
2023-01-25 20:33:30 -0500

    Initial project structure

---
+++ LackeysTests/LackeysTests.swift
@@ -0,0 +1,33 @@
+//
+//  LackeysTests.swift
+//  LackeysTests
+//
+//  Created by jon.ross on 1/25/23.
+//
+
+import XCTest
+@testable import Lackeys
+
+class LackeysTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
