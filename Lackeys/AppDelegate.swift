47779a47effc27847dcab3cc8eafe6f7bb508c02
Jonathan Ross <jon.ross@pathai.com>
2023-01-30 21:20:26 -0500

    Establish menu, callbacks, agent setting

--- Lackeys/AppDelegate.swift
+++ Lackeys/AppDelegate.swift
@@ -11,16 +11,55 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
-
-
+    @IBOutlet weak var menu: NSMenu!
+    private var statusItem : NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        initStatusBar()
+        updateStatusBar()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    }
+    
+    /// Called during initialization to set up the status bar.
+
+    private func initStatusBar() {
+        statusItem = NSStatusBar.system.statusItem(withLength: -1)
+        statusItem.menu = self.menu
+        statusItem.highlightMode = true
     }
 
-
+    /// Called whenever there is a state change that requires changing the status bar.
+    
+    private func updateStatusBar() {
+        statusItem.button?.title = "M?"
+        /*
+        switch keyTap.state {
+            case .manuallyDisabled:
+                statusItem.button?.title = "L-"
+            case .disabledPendingAccess:
+                statusItem.button?.title = "L?"
+            case .disabledDueToError:
+                statusItem.button?.title = "L!"
+            case .enabled:
+                statusItem.button?.title = "L+"
+        }
+        */
+    }
+    
+   @IBAction func enableFromMenu(_ sender: Any) {
+        print("enabled")
+        updateStatusBar()
+    }
+    
+    @IBAction func disableFromMenu(_ sender: Any) {
+        print("disabled")
+        updateStatusBar()
+    }
+    
+    @IBAction func quitFromMenu(_ sender: Any) {
+        NSApplication.shared.terminate(nil)
+    }
 }
 
